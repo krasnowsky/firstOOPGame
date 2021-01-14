842d3fee3de16f6da21fef93f549d38bfbba7a78
krasnowsky <wojtek.ref2@gmail.com>
2021-01-15 00:05:23 +0100

    New class added and assets uploaded

---
+++ include/Snorlax.h
@@ -0,0 +1,8 @@
+//
+// Created by wszystko_ok on 15.01.2021.
+//
+
+#ifndef GAME_SNORLAX_H
+#define GAME_SNORLAX_H
+
+#endif //GAME_SNORLAX_H
---
+++ src/Snorlax.cpp
@@ -0,0 +1,4 @@
+//
+// Created by wszystko_ok on 15.01.2021.
+//
+
